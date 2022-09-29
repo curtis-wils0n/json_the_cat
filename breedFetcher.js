6dd724827a344c634f336fe69ab5aa36735d8352
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-09-29 21:02:19 +0000

    Included tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,14 @@
-const args = process.argv.slice(2);
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, function(error, response, body) {
-  try {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-    console.log(typeof data);
-  } catch (error) {
-    console.log('Error encountered: ', error);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    try {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    } catch (error) {
+      callback(error, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
